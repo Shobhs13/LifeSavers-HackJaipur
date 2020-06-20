f936d142d4787cabfaac999ba307b20ccf77630a
Tanishq Bhala <tanishq@LAPTOP-TA8HKP8U.localdomain>
2020-06-20 13:25:28 +0530

    adding app

---
+++ Lifesaver/app/src/main/java/com/example/lifesaver/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lifesaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+//  demo://dev-lvynb9s5.eu.auth0.com/android/YOUR_APP_PACKAGE_NAME/callbackc
+
+//demo://dev-lvynb9s5.eu.auth0.com/android/YOUR_APP_PACKAGE_NAME/callback
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Lifesaver/app/src/main/java/com/example/lifesaver/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.lifesaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        }, 2000);
+
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+}
