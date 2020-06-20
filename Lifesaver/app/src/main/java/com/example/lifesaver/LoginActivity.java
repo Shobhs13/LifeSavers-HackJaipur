2c560c5ff71bb6c7e9903bdd9923c20bd4ccb599
bhall <bhalla.tanishq2000@gmail.com>
2020-06-21 04:14:14 +0530

    login and register integrated

--- Lifesaver/app/src/main/java/com/example/lifesaver/LoginActivity.java
+++ Lifesaver/app/src/main/java/com/example/lifesaver/LoginActivity.java
@@ -1,14 +1,49 @@
 package com.example.lifesaver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button button;
+    EditText email, password, confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.login_button);
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        confirmPassword = findViewById(R.id.login_confirm_paspassword);
+
+        button.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login Failed.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            });
+
+        });
+
+
     }
 }
--- Lifesaver/app/src/main/java/com/example/lifesaver/RegisterActivity.java
+++ Lifesaver/app/src/main/java/com/example/lifesaver/RegisterActivity.java
@@ -1,15 +1,51 @@
 package com.example.lifesaver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button registerButton;
+    EditText email, password, confirmPassword, address, number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerButton = findViewById(R.id.register_button);
+        email = findViewById(R.id.register_email);
+        password = findViewById(R.id.register_password);
+        confirmPassword = findViewById(R.id.register_confirm_paspassword);
+        number = findViewById(R.id.register_contact);
+
+
+        registerButton.setOnClickListener( view -> {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Create user Failed. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    Toast.makeText(RegisterActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                }
+            });
+
+        });
+
+
 
 
     }
