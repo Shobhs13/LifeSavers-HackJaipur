130e04e668bb59ce41e544091af9f2ee7dcdd65d
bhall <bhalla.tanishq2000@gmail.com>
2020-06-21 01:08:11 +0530

    update

--- Lifesaver/app/src/main/java/com/example/lifesaver/MainActivity.java
+++ Lifesaver/app/src/main/java/com/example/lifesaver/MainActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-//  demo://dev-lvynb9s5.eu.auth0.com/android/YOUR_APP_PACKAGE_NAME/callbackc
-
-//demo://dev-lvynb9s5.eu.auth0.com/android/YOUR_APP_PACKAGE_NAME/callback
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- Lifesaver/app/src/main/java/com/example/lifesaver/SplashActivity.java
+++ Lifesaver/app/src/main/java/com/example/lifesaver/SplashActivity.java
@@ -2,27 +2,48 @@ package com.example.lifesaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.io.FileReader;
 public class SplashActivity extends AppCompatActivity {
+
+    boolean loginFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Timer().schedule(new TimerTask() {
+        SharedPreferences prefs = getSharedPreferences("login", Context.MODE_PRIVATE);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null) {
+            loginFlag = true;
+
+            //get data things
+
+            //if
+        }
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
+                if(!loginFlag)
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             }
         }, 2000);
-
-        startActivity(new Intent(this, LoginActivity.class));
     }
 }
